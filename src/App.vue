cf5a8e221d840998819388f2df8eebffb43900f5
ValenMorales <valentina.moraless@autonoma.edu.co>
2024-03-15 21:40:57 -0500

    adding create plates feature

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,18 @@
-<script>
-import {ref, computed} from 'vue'
-export default {
-setup(){
-
-const dado1= ref(0);
-const dado2= ref(0);
-
-const resultado = computed(()=> (dado1.value == dado2.value)? "Ganaste":"Perdiste");
-
-const tirarDados =() =>{
-dado1.value= Math.floor(Math.random() * 6+1);
-dado2.value= Math.floor(Math.random() * 6+1);
-}
-return {dado1, dado2, resultado, tirarDados}
-
-}
-
-}
-</script>
-
 <template>
-  <h1>Juego de dados</h1>
-
-  <button @click="tirarDados">Tirar dados</button>
-  <h2>Dado 1: {{ dado1 }}</h2>
-  <h2>Dado 2: {{ dado2 }}</h2>
-
-  <h1>{{ resultado }}</h1>
+  <div>
+    <PlatesForm></PlatesForm>
+  </div>
 </template>
+
+<script>
+import PlatesForm from "./components/PlatesForm.vue"; 
+
+export default {
+  components: {
+    PlatesForm 
+  }
+};
+</script>
 
 <style scoped>
 
---
+++ src/components/PlatesForm.vue
@@ -0,0 +1,56 @@
+<template>
+  <form @submit.prevent="createPlate">
+    <input type="text" v-model="plate.name" placeholder="Plate name" name="name" required/>
+    <input type="number" v-model.number="plate.price" placeholder="Plate price" name="price" required/>
+    <input type="number" v-model.number="plate.stock" placeholder="Number of plates" name="stock" required/>
+    
+    <button type="submit">Add plate</button>
+  </form>
+
+  <h1>Plate list</h1>
+  <div v-for="item in plates" :key="item.id">
+    <h2>{{ item.name }}</h2>
+    <p>Price: {{ item.price }}</p>
+    <p>Stock: {{ item.stock }}</p>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  name: "PlatesForm", 
+  setup() {
+    const plates = ref([]);
+    const plate = ref({
+      id: 0,
+      name: "",
+      price: 0,
+      stock: 0,
+    });
+
+    const createPlate = () => {
+      plate.value.id++;
+      plates.value.push({ ...plate.value });
+      plate.value = {
+        id: plate.value.id,
+        name: "",
+        price: 0,
+        stock: 0,
+      };
+    };
+
+    return {
+      plate,
+      plates,
+      createPlate
+    };
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  background-color: aqua;
+}
+</style>./PlatesForm.vue
